964fe2aa6dec3884154e1b1118881485b07d85a1
kionou <kionoumamadou.00@gmail.com>
2022-02-16 10:08:13 +0000

    debut

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine' , 'ejs')
+app.use(express.static('public'));
+
+app.get('/', (req , res) =>{
+    res.render('index')
+});
+
+app.get('/contact', (req,res)=>{
+ res.render('contact')  
+})
+
+app.listen(7070)
