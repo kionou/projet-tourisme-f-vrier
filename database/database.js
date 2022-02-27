78db4bc549c394aa9caf7d767f060287793e73bd
kionou <kionoumamadou.00@gmail.com>
2022-02-27 12:19:26 +0000

    style

--- database/database.js
+++ database/database.js
@@ -2,9 +2,15 @@ let mysql = require('mysql')
 
 
 let db = mysql.createConnection({
-    host:'192.168.64.2',
-    user:'test',
-    password:'12345',
+    
+    // host:'192.168.64.2',
+    // user:'test',
+    // password:'12345',
+
+    host:'localhost',
+    user:'root',
+    password:'',
+
     database:'tourisme',
 });
 module.exports = db;
--- public/js/main.js
+++ public/js/main.js
@@ -9,48 +9,35 @@ let input = document.querySelectorAll('input')
 // let ville = document.querySelector('.ville');
 // console.log(prenom);
 
-function active(){
-  for (let i = 0; i < input.length; i++) {
-    const element = input[i];
-    console.log(input[i].value);
+// function active(){
+//   for (let i = 0; i < input.length; i++) {
+//     const element = input[i];
+//     console.log(input[i].value);
 
-    if (input[i].value === "") {
-      boutton.disabled = true;
-    } else {
-      boutton.disabled = false;
-    }
+//     if (input[i].value === "") {
+//       boutton.disabled = true;
+//     } else {
+//       boutton.disabled = false;
+//     }
     
+//   }
+// }
+
+// form.addEventListener('focus',(event)=>{
+//   active(event)
+
+// })
+
+
+
+let nav = document.querySelector('nav');
+console.log(nav);
+
+window.addEventListener('scroll',function(){
+  if (window.pageYOffset > 100) {
+    nav.classList.add('bg-dark', 'shadow')
+  } else {
+     nav.classList.remove('bg-dark', 'shadow')
   }
-}
-
-
-
-
-
-
-form.addEventListener('focus',(event)=>{
-  active(event)
-
-
 })
 
-
-$(document).ready(function() {
-
-
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
-
-     //>=, not <=
-    if (scroll >= 100) {
-        $(".navbar").addClass("bg-white");
-    } else {
-      $(".navbar").removeClass("bg-white");
-    }
-});
-  
-});
-
-
-// if (nom.value === "" && prenom.value === "" && email.value === "" && numero.value === ""
-// && ville.value ==="") {
