382c4c3bafe2e6768d3c062c5fe2bf35f83301ac
kionou <kionoumamadou.00@gmail.com>
2022-03-01 17:14:15 +0000

    fin

--- router/reservationRouter.js
+++ router/reservationRouter.js
@@ -3,24 +3,26 @@ let router= express.Router();
 let bodyParser = require('body-parser');
 const db = require('../database/database');
  const { check ,validationResult} = require('express-validator');
-let jsonParser= bodyParser.json();
-let urlencodedParser = bodyParser.urlencoded({extended:false})
 
 
 
+
+router.use(bodyParser.json())
 router.get('/',(req,res) =>{
     res.render('../views/reservation')
 })
 
 
-router.get('/message',(req,res) =>{
-  res.render('../views/message')
-})
+// router.get('/message',(req,res) =>{
+//   res.render('../views/message')
+// })
 
 
-router.get('/message', (req,res) =>{
-  db.query(SELECT * FROM `clients`, (err,resultat)=>{
-          console.log(resultat);
+router.get('/message/:id', (req,res) =>{
+  
+  console.log(req.params.id,"eeeeeeeeeeeee");
+  db.query(`SELECT * FROM clients WHERE id = ${req.params.id}`, (err,resultat)=>{
+          console.log("rfrfederrfrfdre",resultat);
           if (err) {
            console.log(err);
           
@@ -33,12 +35,13 @@ router.get('/message', (req,res) =>{
 })
 
 
-router.post('/', urlencodedParser , [
+router.post('/' , [
     check('nom','le nom ne doit pas être vide').exists().isLength({min: 3}),
     check('prenom','le prennom ne doit pas être vide').exists().isLength({min: 3}),
     check('email','le email ne doit pas être vide').isEmail().normalizeEmail(),
     check('numero','le numero ne doit pas être vide').isMobilePhone().isLength({min:10,max: 10}),
 ] , (req,res)=>{
+    console.log(req.body,"aaaaaaaa");
     
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
@@ -49,20 +52,20 @@ router.post('/', urlencodedParser , [
   })  
   } else {
 
-    // res.redirect('/message')
+     res.redirect('reservation/message')
     
-    let{nom,prenom,email,numero,ville} = req.body
-    let sql= "INSERT INTO `clients`(`nom`, `prenom`, `email`, `numero`, `ville`) VALUES (?,?,?,?,?)"; 
-    db.query(sql,[nom,prenom,email,numero,ville],(err,result)=>{
-        if (!err) {
-            console.log("rrrrrrrrttttyyuiuui",result);
-            res.redirect('/message')
+    // let{nom,prenom,email,numero,ville} = req.body
+    // let sql= "INSERT INTO `clients`(`nom`, `prenom`, `email`, `numero`, `ville`) VALUES (?,?,?,?,?)"; 
+    // db.query(sql,[nom,prenom,email,numero,ville],(err,result)=>{
+    //     if (!err) {
+    //         console.log("rrrrrrrrttttyyuiuui",result);
+    //         res.redirect(`reservation/message/${result.insertId}`)
            
       
-        } else {
-            console.log(err);
-        }
-    })
+    //     } else {
+    //         console.log(err);
+    //     }
+    // })
   }
   
 })
