03a67ec232aee532db34c53e9ea3ce164efac189
kionou <kionoumamadou.00@gmail.com>
2022-03-01 08:43:41 +0000

    eeee

--- database/database.js
+++ database/database.js
@@ -3,13 +3,13 @@ let mysql = require('mysql')
 
 let db = mysql.createConnection({
     
-    host:'192.168.64.2',
-    user:'test',
-    password:'12345',
+    // host:'192.168.64.2',
+    // user:'test',
+    // password:'12345',
 
-    // host:'localhost',
-    // user:'root',
-    // password:'',
+    host:'localhost',
+    user:'root',
+    password:'',
 
     database:'tourisme',
 });
